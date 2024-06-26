87a8e7caa26cf92cad6db4be7fa19755f1a12691
Jennifer Her <jenniher0492@gmail.com>
2024-06-26 18:40:57 -0500

    Not done yet

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -14,7 +14,7 @@ function addItem(array) {
         word += array[i];
     } basket.push(word);
     return true;
-} //takes all strings that was split, put them into one string and into an array
+}; //takes all strings that was split, put them into one string and into an array
 
 let newWord = addItem(itemArray);
 console.log(`Basket is ${basket}`);
@@ -28,7 +28,7 @@ function newArray(array) {
     for(word of itemArray) {
         array.push(word);
     } return array;
-} //splitting the items as it was initially in itemArray
+}; //splitting the items as it was initially in itemArray
 
 let newBasket = newArray(basket);
 console.log(`The new list is ${newBasket}`);
@@ -41,7 +41,7 @@ function listItems(array) {
     for(word of array) {
         console.log(`Item needed in bathroom: ${word}`);
     }
-}
+};
 
 listItems(basket);
 console.log('------ lists items separately ------');
@@ -62,43 +62,61 @@ function empty() {
         console.log(basket);
         return 'occupied.';
     } 
-}
+};
 
 empty();
-console.log(`The list is: ${basket}`);
+console.log(`The list is: ${empty()}`);
 
 
 
 console.log('------ STRETCH GOALS ------');
 
+const maxItems = 5; 
+    
+function isFull() {
+    if(basket.length <= maxItems) {
+        return answer = false;
+    } else {
+        return answer = true;
+    }
+};
+
+isFull();
+//console.log(alreadyFull, basket);
+
+console.log('------ CHANGE ADDITEM FUNCTION ------');
+
 
 
 function addItem(item) {
-    const maxItems = 5; //Q1: don't understand why it returns as undefined when called from outside function vs inside. Arrays can be called from outside, so why not variables?
-    function isFull() {
-        if(basket.length <= maxItems) {
-            return false;
-        } else {
-            return true;
-        }
-    } if(isFull() === false) {
+    isFull();
+    if(answer === false) {
         basket.push(item);
         console.log(basket);
         return true;
     } else {
-        console.log(`Cannot add ${item}. ${isFull()}`);
+        console.log(`Cannot add ${item}.`);
         return false;
     }
-}
+};
 
-console.log(`Item was added: ${addItem('mirror')}`);
-console.log(`Item was added: ${addItem('toilet')}`);
-console.log(`Item was added: ${addItem('toothpaste')}`);
-console.log(`Item was added: ${addItem('brush')}`);
-console.log(`Item was added: ${addItem('towel')}`);
-console.log(`Item was added: ${addItem('shower head')}`);
+console.log(`Item was added: ${addNew('mirror')} inside: ${basket}`);
+console.log(`Item was added: ${addNew('toilet')}`);
+console.log(`Item was added: ${addNew('toothpaste')}`);
+console.log(`Item was added: ${addNew('brush')} inside: ${basket}`);
+console.log(`Item was added: ${addNew('towel')}`);
+console.log(`Item was added: ${addNew('showerhead')}`);
+console.log(`Item was added: ${addNew('shampoo')} inside: ${basket}`);
+console.log(basket);
 
+/* function removeItem(item) {
+    basket.indexOf(item);
+    if() {
 
+    } else {
+        return null;
+    }
+} */
 
 // DO NOT MODIFY
 // Used for automated testing
