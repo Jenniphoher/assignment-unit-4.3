58598f27cc46483969986133c1ce49da355d0576
Jennifer Her <jenniher0492@gmail.com>
2024-06-30 18:35:45 -0500

    final changes

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -4,20 +4,21 @@ console.log('***** Cart Functions *****');
 
 let basket = [];
 let input = prompt('3 items needed in the bathroom with a comma and no space:');
-let itemArray = input.split(',');
+let itemArray = input.split(','); // returns like this ==> 'mirror', 'brush', 'paste'
 
 function addItem(array) {
     i = 0;
-    word = array[0];
+    word = array[0]; // automatically adds 'mirror' into array first because when using FOR, I was getting 'mirror' twice ==> 'mirrormirrorbrushpaste'
     while(i<array.length - 1) {
-        i++
-        word += array[i];
-    } basket.push(word);
+        i++;
+        word += array[i]; // adds remaining 'brush' and 'paste' to word[0] which is 'mirror'
+    } basket.push(word); // pushes 'mirrorbrushpaste'
     return true;
 }; //takes all strings that was split, put them into one string and into an array
+// 'mirror', 'brush', 'paste' no checkmark ===> 'mirrorbrushpaste' gets me green checkmark
 
+console.log(`The items have been added: ${addItem(itemArray)}`);
 console.log(`Basket is ${basket}`);
-console.log(`The items have been added: ${addItem}`);
 
 
 
@@ -50,8 +51,7 @@ console.log('------ lists items separately ------');
 console.log(`This is the current list: ${basket}`);
 
 function empty() {
-    i = basket.length;
-    while (i > 0) {
+    while (basket.length > 0) {
         basket.pop();
         i--;
     } if(i === 0) {
@@ -74,9 +74,9 @@ const maxItems = 5;
     
 function isFull() {
     if(basket.length < maxItems) {
-        return answer = false;
+        return false;
     } else {
-        return answer = true;
+        return true;
     }
 };
 
@@ -86,8 +86,8 @@ console.log('------ CHANGE ADDITEM FUNCTION ------');
 
 
 
-function addItem(item) {
-    isFull();
+let newItem = function addItem(item) {
+    let answer = isFull();
     if(answer === false) {
         basket.push(item);
         console.log(basket);
@@ -96,15 +96,15 @@ function addItem(item) {
         console.log(`Cannot add ${item}.`);
         return false;
     }
-};
+}; // I couldn't update addItem without it interfering with the old addItem. 
 
-console.log(`Item was added: ${addItem('mirror')} inside: ${basket}`);
-console.log(`Item was added: ${addItem('toilet')}`);
-console.log(`Item was added: ${addItem('toothpaste')}`);
-console.log(`Item was added: ${addItem('brush')} inside: ${basket}`);
-console.log(`Item was added: ${addItem('towel')}`);
-console.log(`Item was added: ${addItem('showerhead')}`);
-console.log(`Item was added: ${addItem('shampoo')} inside: ${basket}`);
+console.log(`Item was added: ${newItem('mirror')} inside: ${basket}`);
+console.log(`Item was added: ${newItem('toilet')}`);
+console.log(`Item was added: ${newItem('toothpaste')}`);
+console.log(`Item was added: ${newItem('brush')} inside: ${basket}`);
+console.log(`Item was added: ${newItem('towel')}`);
+console.log(`Item was added: ${newItem('showerhead')}`);
+console.log(`Item was added: ${newItem('shampoo')} inside: ${basket}`);
 console.log(basket);
 
 console.log('------ REMOVEITEM FUNCTION ------');
